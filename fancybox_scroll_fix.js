f3c976c8f581ab20df08fb58085bb33c000af84f
agamemnus <agamemnus@gmail.com>
2014-01-04 17:01:22 -0500

    Update fancybox_scroll_fix.js

--- fancybox_scroll_fix.js
+++ fancybox_scroll_fix.js
@@ -1,27 +1,31 @@
+This code combination is (c) 2013 Michael Romanovsky
+Released under MIT license: http://opensource.org/licenses/MIT
+
+// This function is public domain.
 // Merge two objects together.
 // If the primary object has the same key as the secondary object:
 // If both keys' values are function, merge the functions, secondary first.
 // If that key's value is an object, use the primary object's value.
 function merge_objects_with_options (secondary, primary, options) {
-if (typeof options == "undefined") options = {}
+  if (typeof options == "undefined") options = {}
  if (typeof secondary == "undefined") var primary = primary.primary, secondary = primary.secondary
  var new_object = {}
  for (var property in secondary) {new_object[property] = secondary[property]}
  for (var property in primary)   {
-	 if ((options.merge_subfunctions) && (typeof secondary[property] == "function") && (typeof primary[property] == "function")) {
- 	 new_object[property] = function () {
-	 	 var args = Array.prototype.slice.call(arguments)
-	 	 secondary[property].apply (this, args)
-	 	 primary[property]  .apply (this, args)
-	 	}
-			continue
-		}
-	 new_object[property] = primary[property]
-	}
+  if ((options.merge_subfunctions) && (typeof secondary[property] == "function") && (typeof primary[property] == "function")) {
+   new_object[property] = function () {
+    var args = Array.prototype.slice.call(arguments)
+    secondary[property].apply (this, args)
+    primary[property]  .apply (this, args)
+   }
+   continue
+  }
+  new_object[property] = primary[property]
+ }
  return new_object
 }
 
-
+// This function is also public domain.
 function detect_scrollbar_thickness () {
  // Create the measurement node.
  var scrollDiv = document.createElement("div")
@@ -37,7 +41,6 @@ function detect_scrollbar_thickness () {
  document.body.removeChild (scrollDiv)
  return scrollbar_width
 }
-
 
 
 var fancybox_scroll_fix = {
